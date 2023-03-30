44117f81ca45c27435c8af84edee993100b01ae2
Axel Chacon <axel.chacon@tecsup.edu.pe>
2023-03-29 21:52:12 -0500

    Rutas añadidas a index.js

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express = require('express')
 const port = 3000
 const app = express()
 
+//carga de archivos estáticos (css)
+app.use(express.static('static'))
+
 //index.html
 app.get('/',(req, res) => {
     res.sendFile('./static/index.html',{
         root: __dirname
     })
 
+})
+
+//404 error
+app.use((req, res) => {
+    res.status(404).send('Página no encontrada 🤔')
 })
 
 app.listen(port)
